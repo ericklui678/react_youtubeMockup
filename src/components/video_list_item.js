de14d1c0eff52f36bfdaa0c6fff0bd790ea3a995
Erick Lui <erickjin.lui@gmail.com>
2018-02-06 16:57:12 -0800

    added bootstrap markup for VideoListItem

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+// { video } equivalent to video = props.video
 const VideoListItem = ({ video }) => {
-  return <li>{ video.snippet.title }</li>
+  const imgUrl = video.snippet.thumbnails.default.url;
+  return (
+    <li className='list-group-item'>
+      <div className='media'>
+        <img className='mr-3' src={imgUrl}/>
+        <div className='media-body'>
+          <h6 className='mt-0'>{ video.snippet.title }</h6>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
